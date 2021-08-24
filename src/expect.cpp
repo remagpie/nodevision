f1caf1e47e9f2ea24badfb79233521901bbe6c8a
Joonmo Yang <dev@remagpie.com>
2021-08-25 02:57:48 +0900

    Refactor expect.cpp

--- src/expect.cpp
+++ src/expect.cpp
@@ -2,48 +2,32 @@
 
 #include "expect.hpp"
 
-template<>
-Napi::Number expectArg(Napi::Env env, const Napi::CallbackInfo& info, size_t index) {
-	if (info.Length() <= index || !info[index].IsNumber()) {
-		std::ostringstream message;
-		message << index << "th argument is not a Number";
-		Napi::TypeError::New(env, message.str()).ThrowAsJavaScriptException();
+#define EXPECT_ARG_NAPI(type) \
+	template<>\
+	Napi::type expectArg(Napi::Env env, const Napi::CallbackInfo& info, size_t index) {\
+		auto value = info[index];\
+		if (!value.Is##type()) {\
+			std::ostringstream message;\
+			message << index << "th argument is not a(n) " #type;\
+			Napi::TypeError::New(env, message.str()).ThrowAsJavaScriptException();\
+		}\
+		return value.As<Napi::type>();\
 	}
 
-	return info[index].As<Napi::Number>();
-}
-
-template<>
-Napi::String expectArg(Napi::Env env, const Napi::CallbackInfo& info, size_t index) {
-	if (info.Length() <= index || !info[index].IsString()) {
-		std::ostringstream message;
-		message << index << "th argument is not a String";
-		Napi::TypeError::New(env, message.str()).ThrowAsJavaScriptException();
+#define EXPECT_FIELD_NAPI(type) \
+	template<>\
+	Napi::type expectField(Napi::Env env, Napi::Object& obj, const char* name, const char* key) {\
+		auto value = obj.Get(key);\
+		if (!value.Is##type()) {\
+			std::ostringstream message;\
+			message << name << "." << key << " is not a(n) " #type;\
+			Napi::TypeError::New(env, message.str()).ThrowAsJavaScriptException();\
+		}\
+		return value.As<Napi::type>();\
 	}
 
-	return info[index].As<Napi::String>();
-}
+EXPECT_ARG_NAPI(Number)
+EXPECT_ARG_NAPI(String)
+EXPECT_ARG_NAPI(Object)
 
-template<>
-Napi::Object expectArg(Napi::Env env, const Napi::CallbackInfo& info, size_t index) {
-	if (info.Length() <= index || !info[index].IsObject()) {
-		std::ostringstream message;
-		message << index << "th argument is not an Object";
-		Napi::TypeError::New(env, message.str()).ThrowAsJavaScriptException();
-	}
-
-	return info[index].As<Napi::Object>();
-}
-
-template<>
-Napi::Number expectField(Napi::Env env, Napi::Object& obj, const char* name, const char* key) {
-	auto value = obj.Get(key);
-
-	if (!value.IsNumber()) {
-		std::ostringstream message;
-		message << name << "." << key << " is not a Number";
-		Napi::TypeError::New(env, message.str()).ThrowAsJavaScriptException();
-	}
-
-	return value.As<Napi::Number>();
-}
+EXPECT_FIELD_NAPI(Number)
